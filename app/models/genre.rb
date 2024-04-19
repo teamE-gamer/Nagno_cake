8c664cf969bb3f149cba6c809ba4836376419c4e
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-04-19 01:12:38 +0000

    ジャンル試作2

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ApplicationRecord
 
   has_many :items
-#上記のitemとの関係って商品一個に対してジャンルが複数じゃない・・・？
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   has_many :cart_items
   has_many :order_details
   belongs_to :genres
- #上記のgenresとの関係って商品一個に対してジャンルが複数じゃない・・・？
+
   has_one_attached :image
 
 

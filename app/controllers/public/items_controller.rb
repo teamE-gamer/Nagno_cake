fb62752c391a11a783fef43e2b05dbd0ac8bd3e2
HomareYamamoto <159757552+HomareYamamoto@users.noreply.github.com>
2024-04-20 02:57:50 +0000

    顧客側商品一覧ページ完成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,21 @@
 class Public::ItemsController < ApplicationController
 
   def index
+    @items=Item.page(params[:page])
+
   end
 
   def show
+    @item=Item.find(params[:id])
+    tax_cal = @item.price * 1.1
+    @price_with_tax=tax_cal.floor
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price, :status,:genre_id)
+  end
+
 
 end

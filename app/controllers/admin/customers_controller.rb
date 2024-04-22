8d5bf0b5a6592e8f4ee40c6a264cda709b557b69
HomareYamamoto <159757552+HomareYamamoto@users.noreply.github.com>
2024-04-22 11:08:38 +0000

    管理者側顧客機能・商品・顧客バリデーション簡易設定

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,9 +10,16 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer=Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer), notice: "会員情報を更新しました。"
+    else
+      render :edit
+    end
   end
 
 
@@ -26,7 +33,8 @@ class Admin::CustomersController < ApplicationController
                                        :post_code,
                                        :address,
                                        :telephone_number,
-                                       :email
+                                       :email,
+                                       :is_active
                                        )
     end
 
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,6 +8,15 @@ class Customer < ApplicationRecord
   has_many :cart_items
   has_many :orders
 
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :post_code, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true
+  validates :email, presence: true
+
   def active_for_authentication?
     super && (is_active == true)
   end

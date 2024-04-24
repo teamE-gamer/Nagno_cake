b5269d6d9d711f6b1ea14238857aff144f16f4f0
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-04-24 10:46:47 +0000

    注文制作ステータス試作中

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,7 +1,24 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
-    
+  @order_detail = OrderDetail.find(params[:id])
+  # ステータスが送信されてきたデータを整数に変換
+  params[:order_detail][:making_status] = params[:order_detail][:making_status].to_i
+  if @order_detail.update(order_detail_params)
+    # ステータスが更新された場合の処理
+    # 例えば、更新後のステータスをメッセージとして表示するなど
+    redirect_to admin_order_path
+  else
+    # ステータスの更新に失敗した場合の処理
+    render :edit
   end
+  end
+
+private
+
+def order_detail_params
+  params.require(:order_detail).permit(:making_status,:amount,:price,:item_id,:order_id)
+end
+  
 
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,4 +2,7 @@ class OrderDetail < ApplicationRecord
 
   belongs_to :order
   belongs_to :item
+  
+  
+  enum making_status: {着手不可: 0, 製作中: 1, 発送準備中: 2, 発送済: 3},_default: :着手不可
 end
